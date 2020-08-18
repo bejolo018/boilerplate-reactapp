c9a96cff4d7c9fc192fcc24cee5cef05d967f573
Benjamin Lopez <47269255+bejolo018@users.noreply.github.com>
2020-08-17 21:47:34 -0500

    created animated navigation menu with useState hook and react-spring, and styled with tailwind, implemented react-router to have multiple pages

--- boilerplate/src/App.js
+++ boilerplate/src/App.js
@@ -1,12 +1,29 @@
 import React from 'react'
 import Header from './Components/Header'
 import Footer from './Components/Footer'
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Link
+} from 'react-router-dom'
 
 function App() {
   return (
     <div>
+      <Router>
       <Header />
+      <Switch>
+        <Route exact path="/">
+          <h1 className="font-bold text-2x1"> This is the HomePage</h1>
+        </Route>
+        <Route exact path="/about">
+          <h1 className="font-bold text-2x1"> About Us</h1>
+        </Route>
+      </Switch>
       <Footer />
+      </Router>
+
     </div>
   );
 }
